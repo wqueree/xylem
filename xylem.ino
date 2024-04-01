5a8db55085ff3c886013b7a40629dc8e3094969a
Will Queree <wqueree@gmail.com>
2024-04-01 20:31:21 +0100

    Removed duplicated condition block.

--- xylem.ino
+++ xylem.ino
@@ -13,22 +13,12 @@ void open_valve() {}
 void close_valve() {}
 
 
-void water(uint8_t target_moisture) {
-  open_valve();
+void loop() {
+  uint8_t target_moisture = 80;
   uint8_t current_moisture = read_moisture();
   while (current_moisture < target_moisture) {
     output_moisture(current_moisture);
     current_moisture = read_moisture();
-  }
-  close_valve();
-}
-
-
-void loop() {
-  uint8_t target_moisture = 80;
-  uint8_t current_moisture = read_moisture();
-  if (current_moisture < target_moisture) {
-    water(target_moisture);
   }
   output_moisture(current_moisture);
 }
