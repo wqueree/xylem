230627766c42dc4d6ec3391ddf10825020f4c7eb
Will Queree <wqueree@gmail.com>
2024-04-01 20:22:05 +0100

    Added boilerplate.

---
+++ xylem.ino
@@ -0,0 +1,32 @@
+void setup() {}
+
+
+void output_moisture(uint8_t moisture) {}
+
+
+uint8_t read_moisture() {}
+
+
+void open_valve() {}
+
+
+void close_valve() {}
+
+
+void water(uint8_t current_moisture, uint8_t target_moisture) {
+  open_valve();
+  while (current_moisture < target_moisture) {
+    output_moisture(current_moisture);
+  }
+  close_valve();
+}
+
+
+void loop() {
+  uint8_t target_moisture = 80;
+  uint8_t current_moisture = read_moisture();
+  if (current_moisture < target_moisture) {
+    water(current_moisture, target_moisture);
+  }
+  output_moisture(current_moisture);
+}
