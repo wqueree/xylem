6c4dfa1a48f902773a287daca835a3b507b214ac
Will Queree <wqueree@gmail.com>
2024-04-01 20:39:21 +0100

    4 space indentation.

--- xylem.ino
+++ xylem.ino
@@ -5,7 +5,7 @@ void output_moisture(uint8_t moisture) {}
 
 
 uint8_t read_moisture() {
-  return 0;
+    return 0;
 }
 
 
@@ -16,11 +16,11 @@ void close_valve() {}
 
 
 void loop() {
-  uint8_t target_moisture = 80;
-  uint8_t current_moisture = read_moisture();
-  while (current_moisture < target_moisture) {
+    uint8_t target_moisture = 80;
+    uint8_t current_moisture = read_moisture();
+    while (current_moisture < target_moisture) {
+        output_moisture(current_moisture);
+        current_moisture = read_moisture();
+    }
     output_moisture(current_moisture);
-    current_moisture = read_moisture();
-  }
-  output_moisture(current_moisture);
 }
