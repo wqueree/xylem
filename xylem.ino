a39cf3a61c49b8373df9eabbc9a191cf8d26a804
Will Queree <wqueree@gmail.com>
2024-04-01 20:28:38 +0100

    Updated water()

--- xylem.ino
+++ xylem.ino
@@ -13,10 +13,12 @@ void open_valve() {}
 void close_valve() {}
 
 
-void water(uint8_t current_moisture, uint8_t target_moisture) {
+void water(uint8_t target_moisture) {
   open_valve();
+  uint8_t current_moisture = read_moisture();
   while (current_moisture < target_moisture) {
     output_moisture(current_moisture);
+    current_moisture = read_moisture();
   }
   close_valve();
 }
@@ -26,7 +28,7 @@ void loop() {
   uint8_t target_moisture = 80;
   uint8_t current_moisture = read_moisture();
   if (current_moisture < target_moisture) {
-    water(current_moisture, target_moisture);
+    water(target_moisture);
   }
   output_moisture(current_moisture);
 }
